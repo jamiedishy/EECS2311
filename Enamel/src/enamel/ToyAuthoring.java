18c8c6dc13ce80237d825cbca814327b03f83f3b
jamiedishy <jamiedishy@gmail.com>
2018-01-15 11:49:44 -0500

    testpush2

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -3,7 +3,6 @@ package enamel;
 public class ToyAuthoring {
 
     public static void main(String[] args) { 	
-    	//test push!
     	    ScenarioParser s = new ScenarioParser(true);
     	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
     }
