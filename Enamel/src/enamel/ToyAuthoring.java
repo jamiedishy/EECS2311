b226f1ebefbc7a8f0be386511281fcafb2487a92
jamiedishy <jamiedishy@gmail.com>
2018-02-12 11:38:04 -0500

    Test

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,27 @@
 package enamel;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
 public class ToyAuthoring {
 
     public static void main(String[] args) { 	
-    	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+    	//Create a file chooser
+  	  JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+  	    
+  	    //In response to a button click:
+  	  int returnVal = fc.showOpenDialog(null);
+  	  
+  	  if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fc.getSelectedFile();
+			System.out.println(selectedFile.getAbsolutePath());
+		}  
+    	
+  	  	ScenarioParser s = new ScenarioParser(true);
+    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 2 + ".txt");
+    	    
+    	  
     }
 }
